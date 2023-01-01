14f3754724c5f34cd3f4ece143341a19a299cfb6
AmitEilam <amiteilam10@gmail.com>
2023-01-01 15:36:32 +0200

    1.1.23 #7

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
-import "./App.css";
 import Navbar from "./Navbar";
-import Home from "./home";
+import Home from "./Home";
 
 function App() {
-  const title = "Welcome to the new blog";
-  const likes = 50;
-  const link = "https://www.google.co.il";
-  return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
-      </div>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Navbar/>
+            <div className="content">
+                <Home/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
